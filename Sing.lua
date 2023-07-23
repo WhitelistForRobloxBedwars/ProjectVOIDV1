daf91e0bbb32714878e54b37a8571f37b9ad5bae
ProjectVOIDV1 <139603508+ProjectVOIDV1@users.noreply.github.com>
2023-07-23 11:34:35 +0700

    Sing.lua

--- Sing.lua
+++ Sing.lua
@@ -1,4 +1,3 @@
--- Modify the Trolled table to prevent potential misuse
 local Trolled = { Enabled = false }
 
 local function sendTrollMessages(messages)
@@ -21,7 +20,6 @@ local Tab = Window:NewTab("Main")
 local Section = Tab:NewSection("Make By Iamfakemario")
 
 Section:NewButton("Sing You're Been Trolled", "", function()
-    -- Instead of enabling Trolled, simply call the function directly
     local messages = {
         "You've been trolled, you've been trolled",
         "Yes, you've probably been told",
@@ -57,13 +55,11 @@ Section:NewButton("Sing Lyrics You're Been Trolled", "", function()
         "And yet you keep on trying, mindlessly replying",
         "You've been trolled, you've been trolled, have a nice day"
     }
-    for _, message in ipairs(messages) do
-        sendChatMessage(message)
-        wait(3)
-    end
+    sendTrollMessages(messages)
 end)
 
 Section:NewButton("Sing Never Gonna Give You Up", "", function()
+    Trolled.Enabled = true
     local messages = {
         "We're no strangers to love",
         "You know the rules and so do I",
@@ -77,209 +73,12 @@ Section:NewButton("Sing Never Gonna Give You Up", "", function()
         "Never gonna make you cry",
         "Never gonna say goodbye",
         "Never gonna tell a lie and hurt you"
-    }
-    for _, message in ipairs(messages) do
-        sendChatMessage(message)
-        wait(3)
-    end
-end)
-
-Section:NewButton("Sing Lyrics Never Gonna Give You Up", "", function()
-    local messages = {
-        "Never Gonna Give U Up By Rick Ashley",
-        "We're no strangers to love",
-        "You know the rules and so do I",
-        "A full commitment's what I'm thinking of",
-        "You wouldn't get this from any other guy",
-        "I just wanna tell you how I'm feeling",
-        "Gotta make you understand",
-        "Never gonna give you up",
-        "Never gonna let you down",
-        "Never gonna run around and desert you",
-        "Never gonna make you cry",
-        "Never gonna say goodbye",
-        "Never gonna tell a lie and hurt you"
-    }
-    for _, message in ipairs(messages) do
-        sendChatMessage(message)
-        wait(3)
-    end
-end)
-
-Section:NewButton("Sing Demon Slayer Op (Never Gonna Give You Up)", "", function()
-    Trolled.Enabled = true
-    local messages = {
-        "Give you up",
-        "Let's you down",
-        "Run around and desert you",
-        "Make you cry",
-        "Tell a lie",
-        "Say Goodbye",
-        "YEAH",
-        "We're no strangers to love",
-        "You know the rules and so do I",
-        "A full communication of what i thinking of",
-        "You won't getting from any other guys",
-        "I just wanna tell you",
-        "Tell you how I feeling",
-        "Gotta make you understand",
-        "Don't don't don't don't tell me you're",
-        "You're too blind to see",
-        "Give you up",
-        "Let's you down",
-        "Run around and desert you",
-        "Make you cry",
-        "Tell a lie",
-        "Say Goodbye",
-        "**Japan Intro**",
-        "NEVER GONNA GIVE YOU UP",
-        "AND NEVER GOTTA LET'S YOU DOWN",
-        "AND RUN AROUND AND DESERT YOU",
-        "GONNA MAKE YOU",
-        "MAKE YOU UNDERSTAND",
-        "**End Japan Intro**",
-        "NEVER Gonna",
-        "NEVER Gonna GIVE YOU UP",
-        "NEVER NEVER GOTTA LET'S YOU DOWN",
-        "NEVER GOTTA RUN AROUND AND DESERT YOU",
-        "Gonna make you UNDERSTAND!!"
-    }
-    for _, message in ipairs(messages) do
-        sendChatMessage(message)
-        wait(3)
-    end
-end)
-
-Section:NewButton("Sing Lyrics Demon Slayer Op (Never Gonna Give You Up)", "", function()
-    Trolled.Enabled = true
-    local messages = {
-        "Lyrics=Never Gonna Give U Up Demon Slayer OP By Morgi Memes",
-        "Give you up",
-        "Let's you down",
-        "Run around and desert you",
-        "Make you cry",
-        "Tell a lie",
-        "Say Goodbye",
-        "YEAH",
-        "We're no strangers to love",
-        "You know the rules and so do I",
-        "A full communication of what i thinking of",
-        "You won't getting from any other guys",
-        "I just wanna tell you",
-        "Tell you how I feeling",
-        "Gotta make you understand",
-        "Don't don't don't don't tell me you're",
-        "You're too blind to see",
-        "Give you up",
-        "Let's you down",
-        "Run around and desert you",
-        "Make you cry",
-        "Tell a lie",
-        "Say Goodbye",
-        "**Japan Intro**",
-        "NEVER GONNA GIVE YOU UP",
-        "AND NEVER GOTTA LET'S YOU DOWN",
-        "AND RUN AROUND AND DESERT YOU",
-        "GONNA MAKE YOU",
-        "MAKE YOU UNDERSTAND",
-        "**End Japan Intro**",
-        "NEVER Gonna",
-        "NEVER Gonna GIVE YOU UP",
-        "NEVER NEVER GOTTA LET'S YOU DOWN",
-        "NEVER GOTTA RUN AROUND AND DESERT YOU",
-        "Gonna make you UNDERSTAND!!"
-    }
-    for _, message in ipairs(messages) do
-        sendChatMessage(message)
-        wait(3)
-    end
-end)-- Modify the Trolled table to prevent potential misuse
-local Trolled = { Enabled = false }
-
-local function sendTrollMessages(messages)
-    for _, message in ipairs(messages) do
-        game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(message, "All")
-        wait(3)
-    end
-end
-
--- Wait until the game is loaded
-repeat
-    wait()
-until game:IsLoaded()
-
--- Load the UI Library
-local Library = loadstring(game:HttpGet("https://pastebin.com/raw/vff1bQ9F"))()
-local Window = Library.CreateLib("Sing A Song", "DarkTheme")
-
-local Tab = Window:NewTab("Main")
-local Section = Tab:NewSection("Make By Iamfakemario")
-
-Section:NewButton("Sing You're Been Trolled", "", function()
-    -- Instead of enabling Trolled, simply call the function directly
-    local messages = {
-        "You've been trolled, you've been trolled",
-        "Yes, you've probably been told",
-        "Don't reply to this guy",
-        "He's just trying to get a rise",
-        "Out of you, yes, it's true",
-        "You respond and that's his cue",
-        "To start trouble on the double",
-        "While he strokes his manly stubble",
-        "You've been trolled, you've been trolled",
-        "You should probably just fold",
-        "When the only winning move is not to play",
-        "And yet you keep on trying, mindlessly replying",
-        "You've been trolled, you've been trolled, have a nice day"
     }
     sendTrollMessages(messages)
 end)
 
-Section:NewButton("Sing Lyrics You're Been Trolled", "", function()
-    local messages = {
-        "You've been trolled by idk (Really Idk Who)",
-        "You've been trolled, you've been trolled",
-        "Yes, you've probably been told",
-        "Don't reply to this guy",
-        "He's just trying to get a rise",
-        "Out of you, yes, it's true",
-        "You respond and that's his cue",
-        "To start trouble on the double",
-        "While he strokes his manly stubble",
-        "You've been trolled, you've been trolled",
-        "You should probably just fold",
-        "When the only winning move is not to play",
-        "And yet you keep on trying, mindlessly replying",
-        "You've been trolled, you've been trolled, have a nice day"
-    }
-    for _, message in ipairs(messages) do
-        sendChatMessage(message)
-        wait(3)
-    end
-end)
-
-Section:NewButton("Sing Never Gonna Give You Up", "", function()
-    local messages = {
-        "We're no strangers to love",
-        "You know the rules and so do I",
-        "A full commitment's what I'm thinking of",
-        "You wouldn't get this from any other guy",
-        "I just wanna tell you how I'm feeling",
-        "Gotta make you understand",
-        "Never gonna give you up",
-        "Never gonna let you down",
-        "Never gonna run around and desert you",
-        "Never gonna make you cry",
-        "Never gonna say goodbye",
-        "Never gonna tell a lie and hurt you"
-    }
-    for _, message in ipairs(messages) do
-        sendChatMessage(message)
-        wait(3)
-    end
-end)
-
 Section:NewButton("Sing Lyrics Never Gonna Give You Up", "", function()
+    Trolled.Enabled = true
     local messages = {
         "Never Gonna Give U Up By Rick Ashley",
         "We're no strangers to love",
@@ -295,15 +94,13 @@ Section:NewButton("Sing Lyrics Never Gonna Give You Up", "", function()
         "Never gonna say goodbye",
         "Never gonna tell a lie and hurt you"
     }
-    for _, message in ipairs(messages) do
-        sendChatMessage(message)
-        wait(3)
-    end
+    sendTrollMessages(messages)
 end)
 
 Section:NewButton("Sing Demon Slayer Op (Never Gonna Give You Up)", "", function()
     Trolled.Enabled = true
     local messages = {
+        "Demon Slayer Never Gonna Give You Up By Morgi Memes",
         "Give you up",
         "Let's you down",
         "Run around and desert you",
@@ -339,16 +136,12 @@ Section:NewButton("Sing Demon Slayer Op (Never Gonna Give You Up)", "", function
         "NEVER GOTTA RUN AROUND AND DESERT YOU",
         "Gonna make you UNDERSTAND!!"
     }
-    for _, message in ipairs(messages) do
-        sendChatMessage(message)
-        wait(3)
-    end
+    sendTrollMessages(messages)
 end)
 
 Section:NewButton("Sing Lyrics Demon Slayer Op (Never Gonna Give You Up)", "", function()
     Trolled.Enabled = true
     local messages = {
-        "Lyrics=Never Gonna Give U Up Demon Slayer OP By Morgi Memes",
         "Give you up",
         "Let's you down",
         "Run around and desert you",
@@ -384,8 +177,5 @@ Section:NewButton("Sing Lyrics Demon Slayer Op (Never Gonna Give You Up)", "", f
         "NEVER GOTTA RUN AROUND AND DESERT YOU",
         "Gonna make you UNDERSTAND!!"
     }
-    for _, message in ipairs(messages) do
-        sendChatMessage(message)
-        wait(3)
-    end
+    sendTrollMessages(messages)
 end)
