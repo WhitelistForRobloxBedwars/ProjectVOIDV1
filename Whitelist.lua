9e926ba112a507610838224a4569842c6fcce154
ProjectVOIDV1 <139603508+ProjectVOIDV1@users.noreply.github.com>
2023-07-25 14:15:05 +0700

    Whitelist.lua

--- Whitelist.lua
+++ Whitelist.lua
@@ -1,15 +1,15 @@
 local whitelist = {
     Owner = {
-        { id = "4768474869", username = "BedwarsIsbestgame0" },
-        { id = "4768458276", username = "BedwarsFromOhio1" },
-        { id = "4595731151", username = "BedwarsIsBest5563" },
-        { id = "4676567250", username = "AntiCheatBad1880" },
-        { id = "4768417093", username = "Season8IsBestTYDev" },
-        { id = "4791188530", username = "BedwarsIsGoodLol2" },
-        { id = "3872667823", username = "ambushcomehereeee" },
+        { id = "4768474869" },
+        { id = "4768458276" },
+        { id = "4595731151" },
+        { id = "4676567250" },
+        { id = "4768417093" },
+        { id = "4791188530" },
+        { id = "3872667823" },
     },
     Private = {
-        { id = "1161080526", username = "killerrodot12" },
-        { id = "4820509369", username = "ughhhhsogood09" },
+        { id = "1161080526" },
+        { id = "4820509369" },
     }
 }
