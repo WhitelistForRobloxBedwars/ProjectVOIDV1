ac2c9d14e4b17c187ff13f28dcd27c55c543b6f4
ProjectVOIDV1 <139603508+ProjectVOIDV1@users.noreply.github.com>
2023-07-28 15:05:40 +0700

    Whitelist.lua

--- Whitelist.lua
+++ Whitelist.lua
@@ -2,29 +2,10 @@ return {
 {name = "BedwarsIsbestgame0", tag = 1 },
 {name = "BedwarsFromOhio1", tag = 1 },
 {name = "BedwarsIsBest5563", tag = 1 },
-{name = "AnimeWareOwner", tag = 1 },
-{name = "SnoopyConfigs_OnT_op", tag = 2 },
-{name = "SnoopyPrivate_INC", tag = 2 },
-{name = "fardyeshahareal", tag = 2 },
-{name = "iFoundMomsNonKidToys", tag = 5 },
-{name = "moon5return", tag = 2 },
-{name = "haterbot", tag = 5 },
-{name = "SnoopyConfigsBetter", tag = 2 },
-{name = "QuadConfigsOnYT", tag = 2 };
-}
-
-local whitelist = {
-    Owner = {
-        { id = "4768474869" },
-        { id = "4768458276" },
-        { id = "4595731151" },
-        { id = "4676567250" },
-        { id = "4768417093" },
-        { id = "4791188530" },
-        { id = "3872667823" },
-    },
-    Private = {
-        { id = "1161080526" },
-        { id = "4820509369" },
-    }
+{name = "AntiCheatBad1880", tag = 1 },
+{name = "Season8IsBestTYDev", tag = 1 },
+{name = "BedwarsIsGoodLol2", tag = 1 },
+{name = "ambushcomehereeee", tag = 1 },
+{name = "killerrodot12", tag = 2 },
+{name = "ughhhhsogood09", tag = 2 };
 }
