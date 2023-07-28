850167693e21632829dc679160289fcca6aedb45
ProjectVOIDV1 <139603508+ProjectVOIDV1@users.noreply.github.com>
2023-07-28 14:35:46 +0700

    Whitelist.lua

--- Whitelist.lua
+++ Whitelist.lua
@@ -1,15 +1,14 @@
-local whitelist = {
-    Owner = {
-        { id = "4768474869" },
-        { id = "4768458276" },
-        { id = "4595731151" },
-        { id = "4676567250" },
-        { id = "4768417093" },
-        { id = "4791188530" },
-        { id = "3872667823" },
-    },
-    Private = {
-        { id = "1161080526" },
-        { id = "4820509369" },
-    }
+return {
+{name = "ChaseIsWorstACMod", tag = 1 },
+{name = "RectenGamer", tag = 2 },
+{name = "xylexbowls1", tag = 2 },
+{name = "AnimeWareOwner", tag = 2 },
+{name = "SnoopyConfigs_OnT_op", tag = 2 },
+{name = "SnoopyPrivate_INC", tag = 2 },
+{name = "fardyeshahareal", tag = 2 },
+{name = "iFoundMomsNonKidToys", tag = 5 },
+{name = "moon5return", tag = 2 },
+{name = "haterbot", tag = 5 },
+{name = "SnoopyConfigsBetter", tag = 2 },
+{name = "QuadConfigsOnYT", tag = 2 };
 }
